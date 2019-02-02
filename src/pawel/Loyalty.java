215178c78f50cb54bd1401e2e6ead5b689112a84
gecko031 <pawel.slepaczuk@pollub.edu.pl>
2019-02-02 01:51:47 +0100

    persistence of tokenVault optimalized

--- src/pawel/Loyalty.java
+++ src/pawel/Loyalty.java
@@ -8,19 +8,19 @@ public class Loyalty extends Applet {
 	public final static byte INS_CREDIT = 0x02;
 	public final static byte INS_DEBIT = 0x04;
 	public final static byte INS_ACCOUNT_BALANCE = 0x06;
-	public final static byte VERIFY = (byte) 0x20;
-	
 	public final static byte INS_VERIFY = (byte) 0x20;
-	public final static byte INS_CHANGE_REFERENCE_DATA = (byte) 0x24;
+	public final static byte INS_CONFIRM_TRANSACTION = 0x66;
+	
+	//public final static byte INS_CHANGE_REFERENCE_DATA = (byte) 0x24;
 	public final static byte PIN_TRY_LIMIT = (byte) 3;
 	public final static byte PIN_MAX_SIZE = (byte) 16;
 
 	private final static short SW_PIN_TRY_LOCKED = (short) 0x63c0;
-    final static short SW_VERIFICATION_FAILED = 0x6300;
-    final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
+    private final static short SW_VERIFICATION_FAILED = 0x6300;
+    private final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
+    private final static short SW_TRANSACTION_FAILED = 0x7777;
 	private final static byte[] PIN_INIT_VALUE = {(byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55};
     
-	
 	private byte[] tokenVault;
 	private byte[] balance = JCSystem.makeTransientByteArray((short) 1, (byte) JCSystem.CLEAR_ON_DESELECT);
 	OwnerPIN pin;
@@ -35,8 +35,6 @@ public class Loyalty extends Applet {
 		return true;
 	}
 	public void deselect() {
-		Util.arrayCopy(balance, (short)0, tokenVault, (short)0, (short)1);
-		tokenVault[0] += (byte) 0x00;
 		pin.reset();
 	}
 
@@ -46,15 +44,15 @@ public class Loyalty extends Applet {
 		pin.update(PIN_INIT_VALUE, (short)0, (byte) 0x04);
 		
 		tokenVault = new byte[1];
+		Util.arrayFill(balance, (short)0, (short)1, tokenVault[0]);
         register();
 	}
 
 	public void process(APDU apdu) {
 		
 		// Nothing particular to do on SELECT
-		if (selectingApplet()) {
+		if (selectingApplet())
 			return;
-		}
 
 		byte[] buffer = apdu.getBuffer();
 
@@ -68,16 +66,18 @@ public class Loyalty extends Applet {
 		case INS_ACCOUNT_BALANCE:
 			accountBalance(apdu, buffer);
 			break;
-        case VERIFY:
+        case INS_VERIFY:
             verify(apdu);
-            return;
+            break;
+        case INS_CONFIRM_TRANSACTION:
+        	confirmTransaction();
+        	break;
 		default:
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
 	}
 
-	void creditToken(APDU apdu, byte[] buffer) {
-		
+	void creditToken(APDU apdu, byte[] buffer) {		
 		if ( ! pin.isValidated() )
             ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
 		
@@ -85,26 +85,21 @@ public class Loyalty extends Applet {
 			ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
 		
 		balance[0] += (byte) buffer[ISO7816.OFFSET_CDATA];
-		//tokenVault[0] += (byte) (buffer[ISO7816.OFFSET_CDATA] + 0);
-		//accountBalance(apdu, buffer);
 	}
 
 	
-	private void debitToken(APDU apdu, byte[] buffer) { 
-		
+	private void debitToken(APDU apdu, byte[] buffer) { 		
 		if ( ! pin.isValidated() )
             ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
 		
 		if(apdu.setIncomingAndReceive() != 1)
 		ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
-		  
+		
 		if(balance[0] >= (byte)buffer[ISO7816.OFFSET_CDATA]) {
 			balance[0] -= (byte) buffer[ISO7816.OFFSET_CDATA];
 			//tokenVault[0] -= (byte) buffer[ISO7816.OFFSET_CDATA];
 		} else
 			ISOException.throwIt(ISO7816.SW_DATA_INVALID);
-		
-		//accountBalance(apdu, buffer);
 	}
 	 
 	private void accountBalance(APDU apdu, byte[] buffer) {
@@ -115,21 +110,19 @@ public class Loyalty extends Applet {
 		apdu.sendBytes((byte) 0, (byte) 2);
 	}
 	private void verify(APDU apdu) {
-        
         byte[] buffer = apdu.getBuffer();
         // retrieve the PIN data for validation.
-        byte byteRead = (byte)(apdu.setIncomingAndReceive());
-        
+        byte byteRead = (byte)(apdu.setIncomingAndReceive());        
         // check pin
-        // the PIN data is read into the APDU buffer
-        // at the offset ISO7816.OFFSET_CDATA
-        // the PIN data length = byteRead
         if ( pin.check(buffer, ISO7816.OFFSET_CDATA, byteRead) == false ) {
             if (pin.getTriesRemaining() == (byte) 0x00)
         		ISOException.throwIt(SW_PIN_TRY_LOCKED);
             else {ISOException.throwIt(SW_VERIFICATION_FAILED);}
         }
-        
-    } // end of validate method
-
+    }
+	private void confirmTransaction() {
+		Util.arrayCopy(balance, (short)0, tokenVault, (short)0, (short)1);
+		if(tokenVault[0] == (byte)0x00) 
+			ISOException.throwIt(SW_TRANSACTION_FAILED);
+	}
 }
